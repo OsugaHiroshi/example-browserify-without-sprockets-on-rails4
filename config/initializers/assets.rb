3d2483a4224c8efd39b04bb6890d870a375425ed
osuga.hiroshi <osuga.hiroshi@moneyforward.co.jp>
2017-03-16 15:22:17 +0900

    sctipt tag insertion by controller/action name

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.rev_manifest = YAML.load_file('rev-manifest.yml') if File.exist?('rev-manifest.yml')
