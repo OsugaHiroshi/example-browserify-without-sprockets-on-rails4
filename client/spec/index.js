4a85c2cf87213eda0a7d3fceed3566a9beb5e456
osuga.hiroshi <osuga.hiroshi@moneyforward.co.jp>
2017-03-16 14:23:41 +0900

    add power-assert with example test

--- client/spec/index.js
+++ client/spec/index.js
@@ -1,3 +1,13 @@
+const assert = require('power-assert');
 describe('index', () => {
-  it('empty');
+  it('random', () => {
+    const randomValue = ( max, min) => {
+      return Math.floor( Math.random() * (max - min) + min);
+    };
+
+    for(let i = 0 ; i < 1000 ; i++) {
+      const value = randomValue(150, 50);
+      assert.ok( 50 <= value && value < 150 );
+    }
+  });
 });
